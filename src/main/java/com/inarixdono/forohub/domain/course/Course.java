088ca2f8e234c8016e90f0bd8a49f19bc3fd6827
Inarixdono <jenielwtf@gmail.com>
2024-05-22 17:30:25 -0400

    feat: Add Course entity for managing courses

---
+++ src/main/java/com/inarixdono/forohub/domain/course/Course.java
@@ -0,0 +1,38 @@
+package com.inarixdono.forohub.domain.course;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "course")
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    @Enumerated(EnumType.STRING)
+    private Category category;
+    private Boolean status = true;
+
+    public Course(IncomingCourseDTO course) {
+        this.name = course.name();
+        this.category = course.category();
+    }
+
+    public void disableCourse() {
+        this.status = false;
+    }
+}
