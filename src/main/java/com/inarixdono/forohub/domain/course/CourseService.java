1a03dddcf692193321930d7b4b5942fe1ba28e7e
Inarixdono <jenielwtf@gmail.com>
2024-05-22 17:36:30 -0400

    feat: Add transactional annotation to createCourse method in CourseService

--- src/main/java/com/inarixdono/forohub/domain/course/CourseService.java
+++ src/main/java/com/inarixdono/forohub/domain/course/CourseService.java
@@ -3,12 +3,15 @@ package com.inarixdono.forohub.domain.course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import jakarta.transaction.Transactional;
+
 @Service
 public class CourseService {
 
     @Autowired
     private CourseRepository repository;
 
+    @Transactional
     public Course createCourse(IncomingCourseDTO course) {
         return repository.save(new Course(course));
     }
