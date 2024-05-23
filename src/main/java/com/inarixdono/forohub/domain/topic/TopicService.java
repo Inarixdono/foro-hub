cd9aee4a41c7acbd037e4dbb377fafac97db80cd
Inarixdono <jenielwtf@gmail.com>
2024-05-22 23:19:38 -0400

    refactor: Update TopicService to include listTopics, geTopic, updateTopic, and deleteTopic methods

--- src/main/java/com/inarixdono/forohub/domain/topic/TopicService.java
+++ src/main/java/com/inarixdono/forohub/domain/topic/TopicService.java
@@ -1,6 +1,8 @@
 package com.inarixdono.forohub.domain.topic;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.inarixdono.forohub.domain.course.Course;
@@ -27,6 +29,30 @@ public class TopicService {
         User user = userService.findUserById(topic.userId());
         Course course = courseService.findCourseById(topic.courseId());
         return repository.save(new Topic(topic, user, course));
+    }
+
+    @Transactional
+    public Page<TopicDTO> listTopics(Pageable pagination) {
+        return repository.findByStatusTrue(pagination).map(TopicDTO::new);
+    }
+
+    @Transactional
+    public Topic geTopic(Integer id) {
+        return repository.findByIdAndStatusTrue(id).orElseThrow();
+    }
+
+    @Transactional
+    public Topic updateTopic(Integer id, UpdateTopicDTO updateTopicDTO) {
+        Topic topic = repository.getReferenceById(id);
+        topic.update(updateTopicDTO);
+        return topic;
+    }
+
+    @Transactional
+    public Topic deleteTopic(Integer id) {
+        Topic topic = repository.getReferenceById(id);
+        topic.disable();
+        return topic;
     }
 
 }
